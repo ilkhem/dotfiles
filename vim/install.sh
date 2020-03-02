dd70480451f58ecfe7212b960f20c189ccd638b6
Ilyes <ilyes.khemakhem@gmail.com>
2020-03-02 19:22:47 +0000

    fix nvim install.sh for ln

--- vim/install.sh
+++ vim/install.sh
@@ -1,4 +1,21 @@
 set -e
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+user () {
+  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
 link_file () {
   local src=$1 dst=$2
 
@@ -74,4 +91,5 @@ link_file () {
   fi
 }
 
-link_file "$HOME/.nvim" "$HOME/.config/.nvim"
+overwrite_all=false backup_all=false skip_all=false
+link_file "$HOME/.nvim" "$HOME/.config/nvim"
