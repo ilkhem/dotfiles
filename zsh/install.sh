1b9c7f4e27325d1971a0dda607affdf3ddbfeb68
Ilyes <ilyes.khemakhem@gmail.com>
2020-03-07 10:46:20 +0000

    add install of custom plugins

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,3 +3,6 @@
 if [ ! -d "$ZSH" ]; then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
+if [ -d "$ZSH" ]; then
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
