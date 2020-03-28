9983e60364090d3c9b593f989b32b5fbda8cd9aa
Ilyes <ilyes.khemakhem@gmail.com>
2020-03-28 17:26:28 +0000

    fix bug with if statement

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,9 +4,9 @@ if [ ! -d "$ZSH" ]; then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 if [ -d "$ZSH_CUSTOM" ]; then
-        if [! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"]; then
+        if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
                 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
         fi
 fi
 
-[[ -f "zsh/ilyes.zsh-theme"]] && cp "zsh/ilyes.zsh-theme" "$ZSH/themes/"
+cp "zsh/ilyes.zsh-theme" "$ZSH/themes/"
